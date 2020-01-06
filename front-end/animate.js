c66b3c1a5f5d8a36339c6435b4bbd91e42795d73
Victor <victor@gorchilov.com>
2020-01-06 17:56:25 +0200

    add animations

---
+++ front-end/animate.js
@@ -0,0 +1,12 @@
+function animate() {
+    var popup = document.getElementById("popup")
+    popup.classList.toggle("show")
+}
+
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px"
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0"
+}
