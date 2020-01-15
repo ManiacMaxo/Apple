5bb8daf48c24704371527e53733545ccd7c39f63
Ognian Baruh <ognian@baruh.net>
2020-01-15 23:25:59 +0200

    started register validation on flask (not working yet)

--- project/main.py
+++ project/main.py
@@ -1,6 +1,8 @@
-from flask import Flask
-from flask import render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_httpauth import HTTPBasicAuth
+from flask_wtf import FlaskForm
+from wtforms import BooleanField, StringField, PasswordField, validators
+from wtforms.validators import InputRequired
 
 from task import Task
 from user import User
@@ -8,47 +10,71 @@ from user import User
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
+class RegistrationForm(FlaskForm):
+    username = StringField('username')
+    email = StringField('email')
+    password = PasswordField('password')
+    confirm = PasswordField('confirm')
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route("/register", methods = ["GET", "POST"])
 def register():
-    if request.method == 'GET':
-        return render_template('register.html')
+    if request.method == "GET":
+        return render_template("register.html")
     
-    elif request.method == 'POST':
-        values = (
-            None,
-            request.form['username'],
-            request.form['email'],
-            User.hash_password(request.form['password'])
-        )
-        User(*values).create()
-        user = User.find_by_email(request.form['email'])
+    elif request.method == "POST":
 
-        return redirect(url_for('show_profile', id = user.id))
+        flashes = False
+
+        # check if mail exists
+        temp_user = User.find_by_email(request.form["email"])
+        if temp_user:
+            flash("Account with this email already exists!", "email")
+            flashes = True
+
+        # check if passwords match
+        if User.hash_password(request.form["password"]) != User.hash_password(request.form["confirm"]):
+            redirect("/")
+            flashes = True
+
+        if flashes == False:
+            values = (
+                None,
+                request.form["username"],
+                request.form["email"],
+                User.hash_password(request.form["password"])
+            )
+
+            User(*values).create()
+            user = User.find_by_email(request.form["email"])
+
+            return redirect(url_for("show_profile", id = user.id))
+
+        return redirect("/")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods = ["GET", "POST"])
 def login():
-    if request.method == 'GET':
-        return render_template('login.html')
+    if request.method == "GET":
+        return render_template("login.html")
     
-    elif request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+    elif request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
         user = User.find_by_email(email)
                 
         if user and user.verify_password(password):
-            return redirect(url_for('show_profile', id = user.id))
+            return redirect(url_for("show_profile", id = user.id))
 
-        return redirect('/login')
+        return redirect("/login")
 
 
-@app.route('/profile/<int:id>')
+@app.route("/profile/<int:id>")
 def show_profile(id):
     user = User.find_by_id(id)
-    return render_template('profile.html', user=user)
+    return render_template("profile.html", user = user)
